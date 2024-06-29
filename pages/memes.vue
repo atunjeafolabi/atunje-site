3f74f1037d97d0205e2dca5c28907f43005f2345
Atunje <atunje_afolabi@yahoo.com>
2024-06-29 22:52:03 +0200

    adjust top margins of memes

--- pages/memes.vue
+++ pages/memes.vue
@@ -15,67 +15,67 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-5 col-lg-4">
+          <div class="col-md-5 col-lg-4 mt-5">
             <div class="contact_info">
               <img src="images/memes/career-thanksgiving.png" />
             </div>
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/dna_test.png" />
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/odumeje.png" />
           </div>
         </div>
-        <div class="row mt-5">
-          <div class="col-md-5 col-lg-4">
+        <div class="row">
+          <div class="col-md-5 col-lg-4 mt-5">
             <div class="contact_info">
               <img src="images/memes/no_gree_hr.png" />
             </div>
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/rigging-election.png" />
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/relationshi_js_meme.png" />
           </div>
         </div>
-        <div class="row mt-5">
-          <div class="col-md-5 col-lg-4">
+        <div class="row">
+          <div class="col-md-5 col-lg-4 mt-5">
             <div class="contact_info">
               <img src="images/memes/ref_null.png" />
             </div>
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/naija_govt_mad_idea.png" />
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/single-ladies-sql.png" />
           </div>
         </div>
-        <div class="row mt-5">
-          <div class="col-md-5 col-lg-4">
+        <div class="row">
+          <div class="col-md-5 col-lg-4 mt-5">
             <div class="contact_info">
               <img src="images/memes/stack_overflow_keyboard.jpeg" />
             </div>
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/dogmatic.png" />
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/ingress.png" />
           </div>
         </div>
-        <div class="row mt-5">
-          <div class="col-md-5 col-lg-4">
+        <div class="row">
+          <div class="col-md-5 col-lg-4 mt-5">
             <div class="contact_info">
               <img src="images/memes/nigerian-politicians. css.png" />
             </div>
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/node_modules_challenges.png" />
           </div>
-          <div class="col-md-7 col-lg-4">
+          <div class="col-md-7 col-lg-4 mt-5">
             <img src="images/memes/village_people.png" />
           </div>
         </div>
