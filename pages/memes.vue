6666785b4de96ede65bc6ecba9b4681895f98200
Atunje <atunje_afolabi@yahoo.com>
2024-06-29 22:35:21 +0200

    add memes page

---
+++ pages/memes.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="contact" class="section bg_white pp-scrollable">
+    <div class="container">
+      <div class="contact pt_60 md_pl_80">
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <!--	Start Title
+		    				================================-->
+            <div class="main_title pb_60">
+              <h2 class="title color_primary">Some Code Memes</h2>
+              <p class="mt_15">Enjoy !</p>
+            </div>
+            <!--	End Title
+		    				================================-->
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-5 col-lg-4">
+            <div class="contact_info">
+              <img src="images/memes/career-thanksgiving.png" />
+            </div>
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/dna_test.png" />
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/odumeje.png" />
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md-5 col-lg-4">
+            <div class="contact_info">
+              <img src="images/memes/no_gree_hr.png" />
+            </div>
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/rigging-election.png" />
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/relationshi_js_meme.png" />
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md-5 col-lg-4">
+            <div class="contact_info">
+              <img src="images/memes/ref_null.png" />
+            </div>
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/naija_govt_mad_idea.png" />
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/single-ladies-sql.png" />
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md-5 col-lg-4">
+            <div class="contact_info">
+              <img src="images/memes/stack_overflow_keyboard.jpeg" />
+            </div>
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/dogmatic.png" />
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/ingress.png" />
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md-5 col-lg-4">
+            <div class="contact_info">
+              <img src="images/memes/nigerian-politicians. css.png" />
+            </div>
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/node_modules_challenges.png" />
+          </div>
+          <div class="col-md-7 col-lg-4">
+            <img src="images/memes/village_people.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          body: true,
+        },
+      ],
+      title: 'Memes',
+    }
+  },
+}
+</script>
